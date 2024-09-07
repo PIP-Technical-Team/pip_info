95012e0403bf056753c216a1e29cd0204718d456
R.Andres Castaneda Aguilar <acastanedaa@worldbank.org>
2024-09-07 07:50:51 -0400

    testing data and creation file

---
+++ R/iris10_data.R
@@ -0,0 +1,17 @@
+
+getwd()
+iris10 <- iris[1:10,] |> 
+  janitor::clean_names()
+
+dir <-  fs::path("data") |> 
+  fs::dir_create()
+
+fname <- fs::path(dir, "iris")
+
+haven::write_dta(iris10, fs::path(fname, ext = "dta"))
+jsonlite::write_json(iris10, fs::path(fname, ext = "json"))
+yaml::write_yaml(iris10, fs::path(fname, ext = "json"))
+qs::qsave(iris10, fs::path(fname, ext = "qs"))
+fst::write_fst(iris10, fs::path(fname, ext = "fst"))
+readr::write_csv(iris10, fs::path(fname, ext = "csv"))
+
